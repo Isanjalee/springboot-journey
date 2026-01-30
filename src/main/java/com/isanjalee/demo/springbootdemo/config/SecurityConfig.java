ae0880c5287c6d0024ba6d44d6745df251ec447d
Isanjalee Silva <ihnjmsilva152@gmail.com>
2026-01-30 21:36:22 +0530

    Day 06 - Add Spring Security basic authentication

---
+++ src/main/java/com/isanjalee/demo/springbootdemo/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.isanjalee.demo.springbootdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/users/**").authenticated()
+                        .anyRequest().permitAll())
+                .httpBasic(Customizer.withDefaults());
+
+        // For H2 console
+        http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+        return http.build();
+    }
+}
